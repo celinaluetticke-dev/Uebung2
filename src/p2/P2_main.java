2ee42e6980fff9029f4d1f33956d20651c263e56
celinaluetticke-dev <celina.luetticke@gmail.com>
2025-10-28 20:08:27 +0100

    Praesenzaufgabe 2

---
+++ src/p2/P2_main.java
@@ -0,0 +1,16 @@
+package p2;
+
+public class P2_main {
+	public static void main(String[]args) {
+		int i = 18;
+		boolean b = false;
+		
+		if (i > 10) {
+			b = true;
+		}
+		
+		System.out.println("i am Ende = " + i);
+		System.out.println("b am Ende = " + b);
+	}
+
+}
