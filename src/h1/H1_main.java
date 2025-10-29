a4f278eee10e75d6b6d39c9fd2aef35f77c5411c
celinaluetticke-dev <celina.luetticke@gmail.com>
2025-10-29 10:02:33 +0100

    Hausaufgabe 1

---
+++ src/h1/H1_main.java
@@ -0,0 +1,18 @@
+package h1;
+
+public class H1_main {
+	public static void main(String[] args) {
+		int z = 48;
+		
+		// getestet mit:
+		// z=22
+		// z=5
+		
+		if (z > 10) {
+			z = z - 10;
+		}
+		
+		System.out.println("Abschlusswert = " + z);
+	}
+
+}
