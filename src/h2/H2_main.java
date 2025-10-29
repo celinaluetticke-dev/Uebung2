12560684f05517bddc02dc9be91bebdfe76b67bf
celinaluetticke-dev <celina.luetticke@gmail.com>
2025-10-29 13:05:04 +0100

    Hausaufgabe 3 Zwischenstand

--- src/h2/H2_main.java
+++ src/h2/H2_main.java
@@ -5,16 +5,15 @@ public class H2_main {
 		int i = 9;
 		int j = 4;
 		int k = 10;
-		
+
 		// getestet mit:
 		// i=10, j=7,k=2 -> min=2, max=10
-		//i=45,j=600,k=1 -> min=1 ,max=600
-		//i=9,j=4,k=10 -> min=4 ,max=10
-		
+		// i=45,j=600,k=1 -> min=1 ,max=600
+		// i=9,j=4,k=10 -> min=4 ,max=10
+
 		int min = i;
 		int max = k;
-		
-	
+
 		if (j < min) {
 			min = j;
 		}
@@ -27,8 +26,7 @@ public class H2_main {
 		if (j > max) {
 			max = j;
 		}
-		
-		
+
 		System.out.println("Minimum = " + min);
 		System.out.println("Maximum = " + max);
 	}
--- src/h3/H3_main.java
+++ src/h3/H3_main.java
@@ -1,5 +1,46 @@
 package h3;
 
 public class H3_main {
+	public static void main(String[] args) {
+		int i = 200;
+		int j = 300;
+		int k = -10;
+
+		boolean A1 = i > j;
+		boolean A2 = i > 200;
+		boolean A3 = j > 100;
+
+		if (A1 && !A2 && !A3) {
+			k = 1;
+		} else if (A1 && A2 && !A3) {
+			k = 2;
+		} else if (A1 && A2 && A3) {
+			k = 3;
+		} else if (!A1 && !A2 && !A3) {
+			k = 4;
+		}
+
+		System.out.println("i = " + i + "j = " + j);
+
+		if (A1 || A2 || A3) {
+			System.out.println("k = " + k + " -> veraendert");
+		} else
+			System.out.println("k =" + k);
+
+		if (A1) {
+			System.out.println("Ausdruck 1 (i > j) wahr");
+		} else
+			System.out.println("Ausdruck 1 (i > j) falsch");
+
+		if (A2) {
+			System.out.println("Ausdruck 2 (i > 200) wahr");
+		} else
+			System.out.println("Ausdruck 2 (i > 200) falsch");
+
+		if (A3) {
+			System.out.println("Ausdruck 3 (j > 100) wahr");
+		} else
+			System.out.println("Ausdruck 3 (j > 100) falsch");
+	}
 
 }
