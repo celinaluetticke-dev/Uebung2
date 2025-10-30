44bb32dbcbbd1e8b0afd2324003ffcc2352cd08f
celinaluetticke-dev <celina.luetticke@gmail.com>
2025-10-30 19:55:31 +0100

    Hausaufgabe 3 (fertig, weitere Verschachtelungen hinzugefuegt

--- src/p3/P3_main.java
+++ src/p3/P3_main.java
@@ -2,24 +2,37 @@ package p3;
 
 public class P3_main {
 	public static void main(String[] args) {
-		int i = 1;
-		int j = 2;
-		int k = 3;
+		int i = 7;
+		int j = 9;
+		int k = 1;
 		
 		// getestet mit:
 		// i=4, j=2, k=8 -> B, C
-		// i=10, j=9, k=8 -> 
+		// i=10, j=9, k=8 -> Kein Ausdruck zutreffend
 		// i=7, j=9, k=1 -> A
 		
 		if (i < j) {
 			System.out.println("A");
-		}
-		if (i < k) {
+			if (i < k) {
+				System.out.println("B");
+				if (j < k) { 
+					System.out.println("C");
+				}
+			}
+		} else if (i < k) {
 			System.out.println("B");
-		}
-		if (j < k) {
+			
+			if (j < k) {
+					System.out.println("C");
+			}
+				
+		} else if (j < k) {
 			System.out.println("C");
+		} else {
+			System.out.println("Kein Ausdruck zutreffend");
 		}
-	}
-
-}
+	 }
+}		
+		
+		
+		
